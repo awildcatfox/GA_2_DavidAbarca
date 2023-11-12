c54efe2823606d85c0244d0eec6ae46d5435c90e
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-11-11 19:29:40 -0800

    added add and sub

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,6 +37,53 @@ namespace GA_2_DavidAbarca
             // Display full name on button click
             MessageBox.Show(fullName);
         } // btnFullName_Click
+
+        private void txtNumber1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string number1 = txtNumber1.Text;
+            string number2 = txtNumber2.Text;
+            // Parsing my numbers
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            string sign = "+";
+            string equalSign = "=";
+            // Doing math with our new int
+            int result = num1 + num2;
+
+            string equation = $"{number1} {sign} {number2} {equalSign} {result}";
+
+            // Displaying the equation string
+            lblEquation.Content = equation;
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            string number1 = txtNumber1.Text;
+            string number2 = txtNumber2.Text;
+            // Parsing my numbers
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            string sign = "-";
+            string equalSign = "=";
+            // Doing math with our new int
+            int result = num1 - num2;
+
+            string equation = $"{number1} {sign} {number2} {equalSign} {result}";
+
+            // Displaying the equation string
+            lblEquation.Content = equation;
+
+        }
     }
+
+
 }
- 
